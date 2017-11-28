1adc007734f905713252f5c67fc5155153480fa9
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-11-28 21:23:35 +0200

    Update signExtendModule.v

--- signExtendModule.v
+++ signExtendModule.v
@@ -1,9 +1,9 @@
 module signExtend (in ,out);
 
-  input  [15:0] in;
-  output [31:0] out;
+input  [15:0] in;
+output [31:0] out;
 
-  assign out = (in[15] == 1)? {16'hffff , in} : 
-    		       (in[15] == 0)? {16'h0000 ,in}  : 16'hxxxx;
+assign out =  (in[15] == 1)? {16'hffff , in} : 
+              (in[15] == 0)? {16'h0000 ,in}  : 16'hxxxx;
 
 endmodule
