70cd49812787952b9d9b5a01b2edba8e8a8ebef5
Muhammed Magdy Taha <31701182+MuhammedMegz@users.noreply.github.com>
2017-11-28 21:19:02 +0200

    Update signExtendModule.v

--- signExtendModule.v
+++ signExtendModule.v
@@ -1,9 +1,9 @@
 module signExtend (in ,out);
 
-input  [15:0] in;
-output [31:0] out;
+  input  [15:0] in;
+  output [31:0] out;
 
-  assign out = (in[15] == 1)? {4'h1111 , in} :
-  			       (in[15] == 0)? {4'h0000 ,in} : 4'hxxxx;
+  assign out = (in[15] == 1)? {16'hffff , in} : 
+    		       (in[15] == 0)? {16'h0000 ,in}  : 16'hxxxx;
 
 endmodule
