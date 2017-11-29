21c026b676ba6fad2a8fd7232fb1a6b4fd3c75a2
Muhammed Magdy Taha <31701182+MuhammedMegz@users.noreply.github.com>
2017-11-30 01:59:23 +0200

    Update PC.v

--- PC.v
+++ PC.v
@@ -4,15 +4,13 @@ input wire [31:0] nextPC;
 input Reset ,clk;
 output reg [31:0] outPC;
 
+  always @(posedge Reset)
+    begin
+       outPC <= 32'h00000000;
+    end 
+  
 always @(posedge clk)
   begin
-    if(Reset == 1)
-      begin
-        outPC <= 32'h00000000;
-      end
-    else
-      begin
         outPC <= nextPC;
-      end
   end
 endmodule
