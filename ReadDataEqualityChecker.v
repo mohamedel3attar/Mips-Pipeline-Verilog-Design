a1529918ce479a0da886436c80866a60f8268c5b
Muhammed Magdy Taha <31701182+MuhammedMegz@users.noreply.github.com>
2017-11-30 00:12:52 +0200

    Create ReadDataEqualityChecker.v

---
+++ ReadDataEqualityChecker.v
@@ -0,0 +1,9 @@
+module EqualChecker(inData1 ,inData2 ,equalFlag);
+
+input [31:0]  inData1;
+input [31:0]  inData2;
+output equalFlag;
+
+assign equalFlag = (inData1 == inData2) 1'b1 : 1'b0 ;
+
+endmodule
