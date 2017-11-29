f1cbd768d87478aea2dd6a8e7ac3c3f02522ab7e
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-11-30 01:26:23 +0200

    Update InstructionMemory.v

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,30 +1,14 @@
-module InstructionMemory (clk,pc,branch,branchamount,readdata);
-input clk,branch;
-input [5:0] branchamount;
+module InstructionMemory (clk,pc,readdata);
+
+input clk;
 input  [31:0] pc;
- reg [31:0] Imemory [0:1023];
+reg [31:0] Imemory [0:1023];
 output reg [31:0] readdata;
+	
 always @ (posedge clk)
 	
-	begin
-	if (branch==1)
-		readdata <= Imemory[( (pc+4)>>2 )+branchamount];	 
-	else if (branch==0)
-		readdata<= Imemory[pc>>2];
-		
+	begin	 
+	readdata <= Imemory[pc>>2];
 	end			
 		
 endmodule	
-		
-		
-		
-		
-	
-
-	
-
-
-
-
-
-
