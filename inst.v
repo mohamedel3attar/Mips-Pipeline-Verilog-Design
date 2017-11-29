e7c3e215107e4e2d937cbadce43d87bc17f55dc6
mohamed fouad <33027971+mafouadm@users.noreply.github.com>
2017-11-29 19:13:17 +0200

    Add files via upload

---
+++ inst.v
@@ -0,0 +1,35 @@
+module instruction_mem (clk,pc,branch,branchamount,readdata);
+input clk,branch;
+input [5:0] branchamount;
+input  [31:0] pc;
+ reg [31:0] Imemory [0:1023];
+output reg [31:0] readdata;
+always @ (posedge clk)
+	begin
+		if (branch==1)
+	readdata <= Imemory[((pc+4)>>2 )+branchamount];	 
+	else if (branch==0)
+		readdata<= Imemory[pc>>2];
+		
+	end			
+	
+		
+		
+		
+		
+		
+	
+endmodule	
+		
+		
+		
+		
+	
+
+	
+
+
+
+
+
+
