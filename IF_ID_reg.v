b2abcedc6777354463dbb6a58165e034395aa479
Ahmed Awad <34071216+ahmawad96@users.noreply.github.com>
2017-11-30 19:52:31 +0200

    Update IF_ID_reg.v

--- IF_ID_reg.v
+++ IF_ID_reg.v
@@ -1,4 +1,4 @@
-module IF_EX_reg(currentPC_in ,nextPC_in ,currentPC_out ,nextPC_out ,instrIn ,instrOut ,clk ,Reset);
+module IF_ID_reg(currentPC_in ,nextPC_in ,currentPC_out ,nextPC_out ,instrIn ,instrOut ,clk ,Reset);
 
 input wire [31:0] currentPC_in ,nextPC_in ,instrIn;
 input clk ,Reset;
