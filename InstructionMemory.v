961c7a16aa9c72241903b1c4277989003966373b
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-12-01 17:03:35 +0200

    add initial block to read program code from file

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,14 +1,20 @@
-module InstructionMemory (clk,pc,readdata);
+module InstructionMemory(clk,pc,readdata);
 
 input clk;
 input  [31:0] pc;
 reg [31:0] Imemory [0:1023];
 output reg [31:0] readdata;
-	
+
+initial //for testing
+	begin
+		$readmemh("code.txt",Imemory);
+	end
+
+
 always @ (posedge clk)
 	
 	begin	 
 	readdata <= Imemory[pc>>2];
 	end			
 		
-endmodule	
+endmodule
