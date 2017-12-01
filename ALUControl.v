250ddc04880bfdc93dabce0372cbd625ef190c13
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-12-01 23:04:20 +0200

    add support for i-type

--- ALUControl.v
+++ ALUControl.v
@@ -1,13 +1,16 @@
 module ALUControl(clk, ALUControl, ALUOp, funct);
 
 input clk;
-input [1:0] ALUOp;
+input [3:0] ALUOp;
 input [5:0] funct;
 output [3:0] ALUControl;
 
-assign ALUControl = 	(ALUOp == 2'b00) ? 4'b0000:
-			(ALUOp == 2'b01) ? 4'b0001:
-			(ALUOp == 2'b10) ? 
+assign ALUControl = 	(ALUOp == 4'b0000) ? 4'b0000:
+			(ALUOp == 4'b0001) ? 4'b0001:
+			(ALUOp == 4'b0011) ? 4'b0010:
+			(ALUOp == 4'b0100) ? 4'b0011:
+			(ALUOp == 4'b0101) ? 4'b1000:
+			(ALUOp == 4'b0010) ? 
 				((funct == 6'b100000) ? 4'b0000:
 				 (funct == 6'b100010) ? 4'b0001:
 				 (funct == 6'b100100) ? 4'b0010:
