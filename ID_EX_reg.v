07a68e2db3bc9c0fe2ef99709f34ff4e02c377ea
Muhammed Magdy Taha <31701182+MuhammedMegz@users.noreply.github.com>
2017-12-01 23:47:51 +0200

    Update ID_EX_reg.v

--- ID_EX_reg.v
+++ ID_EX_reg.v
@@ -1,6 +1,27 @@
-module ID_EX_reg (nextPC ,ReadData1 ,ReadData2 ,signExtendResult ,);
+module ID_EX_reg ( currentPC_in ,nextPC_in ,ReadData1_in ,ReadData2_in ,SignExtendResult_in ,instr_in
+                  ,currentPC_out ,nextPC_out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,instr_out
+                  ,rsOut ,rtOut ,rdOut ,clk );
+
+  input wire [31:0] currentPC_in ,nextPC_in ,ReadData1_in ,ReadData2_in ,instr_in ,SignExtendResult_in;
+  input wire clk;
+
+  output reg [31:0] currentPC_out ,nextPC_out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,instr_out
+  ,rsOut ,rtOut ,rdOut;
 
 
+
+  always @(posedge clk)
+    begin
+      currentPC_out <= currentPC_out;
+      nextPC_out <= nextPC_out ;
+      ReadData1_out <= ReadData1_out;
+      ReadData2_out <= ReadData2_out;
+      SignExtendResult_out <= SignExtendResult_out;
+      instr_out <= instr_in;
+      rsOut <= instr_in[25:21];
+      rtOut <= instr_in[20:15];
+      rdOut <= instr_in[15:11];
+    end
 
 
 endmodule
