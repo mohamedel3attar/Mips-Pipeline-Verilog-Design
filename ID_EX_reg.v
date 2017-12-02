b17982de56fa2785c6448805ffc953a222b5faad
Ahmed Awad <34071216+ahmawad96@users.noreply.github.com>
2017-12-02 22:30:47 +0200

    Update ID_EX_reg.v

--- ID_EX_reg.v
+++ ID_EX_reg.v
@@ -1,25 +1,32 @@
-module ID_EX_reg ( currentPC_in ,nextPC_in ,ReadData1_in ,ReadData2_in ,SignExtendResult_in ,instr_in
-                  ,currentPC_out ,nextPC_out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,instr_out
-                  ,rsOut ,rtOut ,rdOut ,clk );
+module ID_EX_reg (WBreg,Mreg,EXreg, PCplus4 ,ReadData1_in ,ReadData2_in, ,SignExtendResult_in ,instr_in ,PCplus4out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,rsOut ,rtOut ,rdOut,WBregOut,MregOut,EXregOut ,clk );
+  
+  input wire [1:0] WBreg; //contains RegWrite & MemtoReg 
+  input wire [1:0] Mreg; //contains Memwrite & Memread
+  input wire [3:0] EXreg; //contains ALUSrc, ALUOp & RegDst
+  output reg [1:0] WBregOut;
+  output reg [1:0] MregOut;
+  output reg [3:0] EXregOut;
 
-  input wire [31:0] currentPC_in ,nextPC_in ,ReadData1_in ,ReadData2_in ,instr_in ,SignExtendResult_in;
+  input wire [31:0] PCplus4 ,ReadData1_in ,ReadData2_in ,instr_in ,SignExtendResult_in;
   input wire clk;
 
-  output reg [31:0] currentPC_out ,nextPC_out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,instr_out
+  output reg [31:0] PCplus4out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out
   ,rsOut ,rtOut ,rdOut;
+  
 
   
   always @(posedge clk)
     begin
-      currentPC_out <= currentPC_in;
-      nextPC_out <= nextPC_in ;
+      PCplus4out <= PCplus4;
       ReadData1_out <= ReadData1_in;
       ReadData2_out <= ReadData2_in;
       SignExtendResult_out <= SignExtendResult_in;
-      instr_out <= instr_in;
       rsOut <= instr_in[25:21];
       rtOut <= instr_in[20:15];
       rdOut <= instr_in[15:11];
+      WBregOut<=WBreg;
+      MregOut<=Mreg;
+      EXregOut<=EXreg;
     end
   
 endmodule
