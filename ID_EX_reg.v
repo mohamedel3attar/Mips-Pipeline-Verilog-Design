9bf4b73ce747dc11a1373718d92b8c09158be2c6
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-12-04 00:28:20 +0200

    Update ID_EX_reg.v

--- ID_EX_reg.v
+++ ID_EX_reg.v
@@ -1,18 +1,22 @@
-module ID_EX_reg (WBreg,Mreg,EXreg, PCplus4 ,ReadData1_in ,ReadData2_in, ,SignExtendResult_in ,instr_in ,PCplus4out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,rsOut ,rtOut ,rdOut,WBregOut,MregOut,EXregOut ,clk );
+module ID_EX_reg (RegWrite, MemtoReg, MemWrite, MemRead,ALUSrc, ALUOp, RegDst, PCplus4 ,ReadData1_in ,ReadData2_in,SignExtendResult_in 
+		,regAddresss_in ,PCplus4out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out ,rsOut ,rtOut ,rdOut, RegWriteOut
+		,MemtoRegOut,MemWriteOut, MemReadOut,ALUSrcOut, ALUOpOut, RegDstOut,clk);
   
-  input wire [1:0] WBreg; //contains RegWrite & MemtoReg 
-  input wire [1:0] Mreg; //contains Memwrite & Memread
-  input wire [2:0] EXreg; //contains ALUSrc, ALUOp & RegDst
-  output reg [1:0] WBregOut;
-  output reg [1:0] MregOut;
-  output reg [2:0] EXregOut;
+  input wire RegWrite, MemtoReg;
+  input wire MemWrite, MemRead; 
+  input wire ALUSrc, RegDst;
+  input wire [3:0] ALUOp;
+  output reg RegWriteOut, MemtoRegOut;
+  output reg MemWriteOut, MemReadOut;
+  output reg ALUSrcOut, RegDstOut;
+  output reg [3:0] ALUOpOut;
 
-  input wire [31:0] PCplus4 ,ReadData1_in ,ReadData2_in ,instr_in ,SignExtendResult_in;
+  input wire [31:0] PCplus4 ,ReadData1_in ,ReadData2_in ,SignExtendResult_in;
+  input wire [14:0] regAddresss_in;
   input wire clk;
 
-  output reg [31:0] PCplus4out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out
-  ,rsOut ,rtOut ,rdOut;
-  
+  output reg [31:0] PCplus4out ,ReadData1_out ,ReadData2_out ,SignExtendResult_out;
+  output reg [4:0] rsOut ,rtOut ,rdOut;
 
   
   always @(posedge clk)
@@ -21,12 +25,16 @@ module ID_EX_reg (WBreg,Mreg,EXreg, PCplus4 ,ReadData1_in ,ReadData2_in, ,SignEx
       ReadData1_out <= ReadData1_in;
       ReadData2_out <= ReadData2_in;
       SignExtendResult_out <= SignExtendResult_in;
-      rsOut <= instr_in[25:21];
-      rtOut <= instr_in[20:15];
-      rdOut <= instr_in[15:11];
-      WBregOut<=WBreg;
-      MregOut<=Mreg;
-      EXregOut<=EXreg;
+      rsOut <= regAddresss_in[14:10];
+      rtOut <= regAddresss_in[9:5];
+      rdOut <= regAddresss_in[4:0];
+      RegWriteOut <= RegWrite;
+      MemtoRegOut <= MemtoRegOut;
+      MemWriteOut <= MemWriteOut;
+      MemReadOut <= MemRead;
+      ALUSrcOut <= ALUSrc;
+      ALUOpOut <= ALUOp;
+      RegDstOut <= RegDst;
     end
   
 endmodule
