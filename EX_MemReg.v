f56a5b9d9554ed1a50d1e6eeb8c5f862073db069
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-12-04 00:29:38 +0200

    Update EX_MemReg.v

--- EX_MemReg.v
+++ EX_MemReg.v
@@ -1,17 +1,21 @@
-module EX_MemReg (clk,WBreg,Mreg,ALUresult,writedata,writeReg,WBregOut,MregOut,ALUresultOut,writedataOut,writeRegOut);
+module EX_MemReg (clk,RegWrite, MemtoReg,MemWrite, MemRead,ALUresult,writedata,writeReg,RegWriteOut, MemtoRegOut,MemWriteOut
+			,MemReadOut,ALUresultOut,writedataOut,writeRegOut);
+  
   input clk;
-  input [1:0] WBreg;  //contains RegWrite & MemtoReg 
-  input [1:0] Mreg;  //contains Memwrite & Memread
+  input RegWrite, MemtoReg;
+  input MemWrite, MemRead; 
   input [31:0] ALUresult,writedata;
   input [4:0] writeReg;
-  output reg [1:0] WBregOut,MregOut;
+  output reg RegWriteOut, MemtoRegOut ,MemWriteOut, MemReadOut;
   output reg [31:0] ALUresultOut,writedataOut;
   output reg [4:0] writeRegOut;
   
   always@(posedge clk)
     begin
-      WBregOut<=WBreg;
-      MregOut<=Mreg;
+      RegWriteOut <= RegWrite;
+      MemtoRegOut <= MemtoReg;
+      MemWriteOut <= MemWrite;
+      MemReadOut <= MemRead;
       ALUresultOut<=ALUresult;
       writedataOut<=writedata;
       writeRegOut<=writeReg;
