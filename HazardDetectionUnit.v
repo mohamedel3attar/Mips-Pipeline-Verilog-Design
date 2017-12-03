83910cd6f7f80989fb28e24b7f655150d8c03112
Ahmed Awad <34071216+ahmawad96@users.noreply.github.com>
2017-12-03 22:34:09 +0200

    Update HazardDetectionUnit.v

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -1,9 +1,9 @@
-module Hazard_Detection_Unit (ID_ExMemRead,EX_MemMemRead,ID_Ex_Rt,IF_ID_Instr,holdPC,holdIF_ID,ORgate_In);
+module Hazard_Detection_Unit (ID_ExMemRead,EX_MemMemRead,ID_Ex_Rt,IF_ID_Instr,holdPC,holdIF_ID,muxSelector);
   
   input ID_ExMemRead,EX_MemMemRead;
   input [4:0] ID_Ex_Rt;
   input [31:0] IF_ID_Instr;
-  output reg holdPC, holdIF_ID, ORgate_In;
+  output reg holdPC, holdIF_ID, muxSelector;
   parameter beqOPcode=6'b000100;
   always@(ID_ExMemRead or ID_Ex_Rt or IF_ID_Instr)
     begin
@@ -13,14 +13,14 @@ module Hazard_Detection_Unit (ID_ExMemRead,EX_MemMemRead,ID_Ex_Rt,IF_ID_Instr,ho
             begin
               holdPC<=1;
               holdIF_ID<=1;
-              ORgate_In<=1;
+              muxSelector<=1;
               
             end
           else if(IF_ID_Instr [31:26]==beqOPcode && EX_MemMemRead)
             begin
                holdPC<=1;
               holdIF_ID<=1;
-              ORgate_In<=1;
+              muxSelector<=1;
               
             end
           
@@ -28,7 +28,7 @@ module Hazard_Detection_Unit (ID_ExMemRead,EX_MemMemRead,ID_Ex_Rt,IF_ID_Instr,ho
             begin
                holdPC<=0;
               holdIF_ID<=0;
-              ORgate_In<=0;
+              muxSelector<=0;
               
             end
           
