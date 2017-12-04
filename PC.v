99320570c93276c9c4bff14c1d39af63e2f21eb8
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2017-12-04 20:30:20 +0200

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,19 +1,17 @@
-module PC (nextPC ,outPC ,Reset ,clk,holdPC);
+module PC (nextPC ,outPC ,Reset ,clk, holdPC);
 
 input wire [31:0] nextPC;
-input Reset ,clk,holdPC;
-reg [31:0] currentPC;
+input Reset , clk, holdPC;
 output reg [31:0] outPC;
 
-always@(posedge Reset) currentPC <= 32'h00000000;
+always@(posedge Reset) outPC <= 32'hFFFFFFFC;
   
+
 always @(posedge clk)
   begin
 	  if (holdPC==0) //to support stalls from hazard detection unit
 		  begin
-                   outPC <= currentPC;
-	            #1
-	           currentPC <= nextPC;
+                   outPC <= nextPC;
 		  end
   end
 
